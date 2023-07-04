5df5b3bc02d99ccc41882721e165726243a3f2d0
Sahil Deep Singh <111080289+sahil-deep-singh@users.noreply.github.com>
2023-07-04 00:22:54 -0400

    Add files via upload

---
+++ Sample_Data_Script_Group10.sql
@@ -0,0 +1,138 @@
+--Final Project---
+--Group 10--
+--SAMPLE DATA SCRIPT FILE----
+---MEMBER-1: SIMRAN KAUR---
+---MEMBER-2: ANKITA CHANDEL---
+---MEMBER-3: SAHIL DEEP SINGH---
+
+--DATA INPUT FOR TABLE MEMBERS:
+INSERT ALL
+INTO MEMBERS VALUES(201,'Mark Meritt', 19, 6477897045, 'meritt1@gmail.com', '67 Bathrust Street', 'Teenager')
+INTO MEMBERS VALUES(202,'Mary Kom',14,6479234561,'Mkom1234@gmail.com','97 Heartleaf Crescent','Teenager')
+INTO MEMBERS VALUES(203,'Vinsit Kom',20,1298534561,'Vinkom@gmail.com','53 Sandcherry Crescent','Adult')
+INTO MEMBERS VALUES(204,'TIMI JAISWAL',22,6479345612,'Timjai4@gmail.com','22 George Town','Adult')
+INTO MEMBERS VALUES(205,'Simran Kaur',14,6419044561,'Simkaur@gmail.com','44 Simjil Way','Teenager')
+INTO MEMBERS VALUES(206,'John Smith',24,6167820951,'Johnsmith@gmail.com','64 Missisauga Road','Adult')
+INTO MEMBERS VALUES(207,'Kimson Pitt',21,4432863561,'Kimpitt456@gmail.com','66 Park Avenue','Adult')
+INTO MEMBERS VALUES(208,'Jenny Kim',25,3451189452,'Jenny567@gmail.com','21 Los Angeles','Adult')
+INTO MEMBERS VALUES(209,'Ratan Tata',67,2876394561,'TataRatan00@gmail.com','90 Kiadharnat point','Senior')
+INTO MEMBERS VALUES(210,'PV Sindhu',30,1239992477,'Pvsind111@gmail.com','37 Kalyani Nagar','Adult')
+INTO MEMBERS VALUES(211,'Saina Mirza',47,9675530285,'Saina6550@gmail.com','49 Kenning View','Adult')
+INTO MEMBERS VALUES(212,'Kristen Stewart',29,6422098041,'Kristen9990@gmail.com','58 Castlemere Street','Adult')
+INTO MEMBERS VALUES(213,'Shane Vij',48,5537890461,'ShaneV@gmail.com','78 Kipling Avenue','Adult')
+INTO MEMBERS VALUES(214,'Preeti Sharma',27,5433897100,'Preet2S@gmail.com','57 Martin Groove','Adult')
+INTO MEMBERS VALUES(215,'Pramod Sharma',18,7774329954,'Psharma@gmail.com','33 Krol Bagh','Teenager')
+INTO MEMBERS VALUES(216,'Jannat Malhotra',19,8950196661,'Janmalh17@gmail.com','11 Shastri Colony','Teenager')
+INTO MEMBERS VALUES(217,'Rachna Devi',15,4372613344,'Devirachna67@gmail.com','64 Police Line','Teenager')
+INTO MEMBERS VALUES(218,'Samera Rickon',33,3426667812,'samera4289@gmail.com','99 Kelle Street','Adult')
+INTO MEMBERS VALUES(219,'Aditya Aggarwal',22,6411836761,'Aaggarwal1999@gmail.com','46 Peel Town','Adult')
+INTO MEMBERS VALUES(220,'Peter Robertson',18,7652234095,'Peterpan6657@gmail.com','39 Rain Deer','Teenager')
+INTO MEMBERS VALUES(221,'John Sena',40,6473347781,'Senaajohn@gmail.com','72 Mayfield Junction','Adult')
+INTO MEMBERS VALUES(222,'Roman Reigns',38,1134227561,'Romanrrr@gmail.com','52 Jackson Intersection','Adult')
+INTO MEMBERS VALUES(223,'Lisa Mona',34,6442286640,'Monalisa@gmail.com','76 Queen Marie','Adult')
+INTO MEMBERS VALUES(224,'Terry Pickle',21,2234784561,'Terrypick@gmail.com','93 Heartleaf Fort','Adult')
+INTO MEMBERS VALUES(225,'Daisy Fill',12,6777234461,'Filldaisy4@gmail.com','27 Fennel Peak','Teenager')
+SELECT * FROM DUAL;
+
+--DATA INPUT FOR TABLE BOOKS:
+INSERT ALL
+INTO BOOKS VALUES(1784, 'THE CHRISTMAS TRUCK', 'JUVENILE FICTION', 'Y', 'SB.SINGH')
+INTO BOOKS VALUES(1785,'Keepers Reach','Audio Books','Y','Carla Neggers')
+INTO BOOKS VALUES(1786,'The Desert Prince','Juvenile Fiction','Y','Alisha Sevigny')
+INTO BOOKS VALUES(1787,'Bear Cant Sleep','Picture Books','Y','Jane and Wilson')
+INTO BOOKS VALUES(1788,'Sometimes a Wall','Picture Books','N','Dianne White')
+INTO BOOKS VALUES(1789,'Dear Student','Juvenile Fiction','Y','J.Swart')
+INTO BOOKS VALUES(1790,'The Cellar','Audio Books','Y','Minette Walter')
+INTO BOOKS VALUES(1791,'Blood on Snow','Juvenile Fiction','Y','Patti Smith')
+INTO BOOKS VALUES(1792,'Nyc Shadow','Novels','Y','Mark Teene')
+INTO BOOKS VALUES(1793,'Who Invented Periodic Table?','Non-Fiction','Y','Patti Smith')
+INTO BOOKS VALUES(1794,'On the Move','Non-Fiction','Y','Jane and Wilson')
+INTO BOOKS VALUES(1796,'Eggcellent','Novels','N','Patti Smith')
+INTO BOOKS VALUES(1797,'Hurricane Force','Non-Fiction','Y','Joseph.B')
+INTO BOOKS VALUES(1798,'Mouses Christmas Cookie','Picture Books','Y','Jane and Wilson')
+INTO BOOKS VALUES(1799,'First Class Murder','Picture Books','N','J.Swart')
+INTO BOOKS VALUES(1783,'Paper Heart','Juvenile Fiction','Y','J.Swart')
+INTO BOOKS VALUES(1782,'Planet of the Apes','Adult Fiction','Y','George Alac')
+INTO BOOKS VALUES(1781,'Until Death','Adult Fiction','Y','Jane and Wilson')
+INTO BOOKS VALUES(1770,'The monster Who wasnt','Picture Books','Y','Shelley')
+INTO BOOKS VALUES(1769,'Road Trip','Novels','Y','J.Third')
+INTO BOOKS VALUES(1768,'Amazing Planet on Earth','Non-Fiction','N','Terry')
+INTO BOOKS VALUES(1767,'Good People Here','Adult Fiction','Y','Shelley')
+INTO BOOKS VALUES(1766,'Captain Small Pig','Picture Books','Y','Martin Walde')
+INTO BOOKS VALUES(1765,'Luna','Adult Fiction','Y','Shiv Kumar')
+INTO BOOKS VALUES(1764,'The Night Before','Audio Books','N','Wendy Walker')
+INTO BOOKS VALUES(1763,'First Encyclopedia','Novels','Y','Patti?Smith')
+SELECT * FROM DUAL;
+
+--DATA INPUT FOR TABLE L_EMPLOYEES:
+INSERT ALL
+INTO L_EMPLOYEES VALUES(1, 'JULIE SUE', 'RECEPTIONIST', 25, 6476785842, 'sue.j@gmail.com')
+INTO L_EMPLOYEES VALUES(2, 'SIMRAN KAUR', 'RECEPTIONIST', 24, 6647382912, 'kaursimran@gmail.com')
+INTO L_EMPLOYEES VALUES(3, 'JULIA ANDERSON', 'BOOK-STOCKER', 23, 4372612211, 'ajulia1@gmail.com')
+INTO L_EMPLOYEES VALUES(4, 'RITZ', 'BOOK-STOCKER', 24, 6472317856, 'itzritz@gmail.com')
+INTO L_EMPLOYEES VALUES(5, 'ANDRIYA JONES', 'CLERK', 25, 6476785433, 'jones13@gmail.com')
+INTO L_EMPLOYEES VALUES(6, 'RAJDEEP KAUR', 'CLERK', 27, 6476785555, 'rdeepkaurj@gmail.com')
+INTO L_EMPLOYEES VALUES(7, 'MICHEL COLIN', 'LIBRARY CLEANER', 31, 6411342567, 'colinm6@gmail.com')
+INTO L_EMPLOYEES VALUES(8, 'RAMANDEEP', 'LIBRARY CLEANER', 29, 4372614576, 'deepraman26j@gmail.com')
+INTO L_EMPLOYEES VALUES(9, 'ROMAN SANDELWOOD', 'SECURITY-GAURD', 25, 6473456378, 'woodroman@gmail.com')
+INTO L_EMPLOYEES VALUES(10, 'SAHIL DEEP SINGH', 'EVENT-MANAGER', 21, 4376177064, 'sahilbhangu617@gmail.com')
+SELECT * FROM DUAL;
+
+
+--DATA INPUT FOR TABLE EVENTS:
+INSERT ALL
+INTO EVENTS VALUES('ART AND CRAFT','2022-11-14','14:00:00',10,'Teenager')
+INTO EVENTS VALUES('LANGUAGE SESSION','2022-12-2','15:00:00', 7,'Teenager')
+INTO EVENTS VALUES('BOD MEETING','2022-12-8','16:00:00', 10,'Adult')
+INTO EVENTS VALUES('CHILD PLAY','2022-12-16','11:00:00', 3,'Teenager')
+INTO EVENTS VALUES('READING QUIZ','2022-12-24','18:00:00', 7,'Teenager')
+INTO EVENTS VALUES('MENTAL HEALTH SEMINAR','2022-12-26','9:00:00', 10,'Adult')
+INTO EVENTS VALUES('REASONING QUIZ','2022-12-28','16:00:00', 4,'Teenager')
+INTO EVENTS VALUES('DICTATION COMPETITION','2023-01-3','14:00:00', 2,'Senior')
+INTO EVENTS VALUES('BINGO DAY','2023-01-11','08:00:00', 7,'Teenager')
+INTO EVENTS VALUES('LINKEDIN LEARNING','2023-01-14','10:00:00', 10,'Adult')
+SELECT * FROM DUAL;
+
+--DATA INPUT FOR TABLE FEEDBACK:
+INSERT ALL
+INTO FEEDBACK VALUES(1,'THE CHRISTMAS TRUCK', 201, 'JULIE SUE', 4, 5)
+INTO FEEDBACK VALUES(2,'Until Death', 211, 'SIMRAN KAUR', 3, 4)
+INTO FEEDBACK VALUES(3,'Road Trip', 213, 'RITZ', 5, 2)
+INTO FEEDBACK VALUES(4,'On The Move', 205, 'JULIA ANDERSON', 5, 5)
+INTO FEEDBACK VALUES(5,'luna', 216, 'RAJDEEP KAUR', 5, 4)
+INTO FEEDBACK VALUES(6,'Blood On Snow', 203, 'SAHIL DEEP SINGH', 2, 5)
+INTO FEEDBACK VALUES(7,'First Class Murder', 217, 'ANDRIYA JONES', 3, 3)
+INTO FEEDBACK VALUES(8,'The Desert Prince', 210, 'ROMAN-SANDELWOOD', 5, 5)
+SELECT * FROM DUAL;
+
+--DATA INPUT FOR TABLE BOOKBORROW:
+INSERT ALL
+INTO BOOKBORROW VALUES(1784, 201, '2022-09-06', '2022-09-16', 'meritt1@gmail.com')
+INTO BOOKBORROW VALUES(1798, 211, '2022-09-08', '2022-09-18', 'Saina6550@gmail.com')
+INTO BOOKBORROW VALUES(1788, 215, '2022-09-03', '2022-09-13', 'Psharma@gmail.com')
+INTO BOOKBORROW VALUES(1767, 223, '2022-09-06', '2022-09-16', 'Monalisa@gmail.com')
+INTO BOOKBORROW VALUES(1765, 222, '2022-10-01', '2022-10-10', 'Romanrrr@gmail.com')
+INTO BOOKBORROW VALUES(1782, 214, '2022-09-23', '2022-10-02', 'Preet2S@gmail.com')
+INTO BOOKBORROW VALUES(1787, 224, '2022-09-06', '2022-09-16', 'Terrypick@gmail.com')
+INTO BOOKBORROW VALUES(1799, 207, '2022-08-29', '2022-09-09', 'Kimpitt456@gmail.com')
+INTO BOOKBORROW VALUES(1789, 203, '2022-10-11', '2022-10-20', 'Vinkom@gmail.com')
+INTO BOOKBORROW VALUES(1783, 219, '2022-08-06', '2022-08-16', 'Aaggarwal1999@gmail.com')
+INTO BOOKBORROW VALUES(1796, 210, '2022-10-25', '2022-11-04', 'Pvsind111@gmail.com')
+SELECT * FROM DUAL;
+
+--DATA INPUT FOR TABLE ROOMBOOK:
+INSERT ALL
+INTO ROOMBOOK VALUES(1,'Small Room', 'LANGUAGE SESSION', '2022-07-08', '1:00:00', '2 hr', 201)
+INTO ROOMBOOK VALUES(2,'Small Room', 'BINGO DAY', '2022-07-08', '14:00:00', '1 hr', 205)
+INTO ROOMBOOK VALUES(3,'Small Room', 'BOD MEETING', '2022-10-06', '9:00:00', '1 hr', 219)
+INTO ROOMBOOK VALUES(4,'Activity Room', 'REASONING QUIZ', '2022-09-05', '12:00:00', '1 hr', 225)
+INTO ROOMBOOK VALUES(5,'Activity Room', 'ART AND CRAFT', '2022-07-08', '1:00:00', '2 hr', 217)
+INTO ROOMBOOK VALUES(6,'Silent Room', 'REASONING QUIZ', '2022-08-16', '10:00:00', '1 hr', 220)
+INTO ROOMBOOK VALUES(7,'Tech Room', 'LINKEDIN LEARNING', '2022-08-23', '14:00:00', '2 hr', 222)
+INTO ROOMBOOK VALUES(8,'Main hall', 'MENTAL HEALTH SEMINAR', '2022-07-18', '1:00:00', '2 hr', 214)
+INTO ROOMBOOK VALUES(9,'Medium Room','READING QUIZ', '2022-09-11', '10:00:00', '1 hr',215)
+SELECT * FROM DUAL;
+
+
+
+
